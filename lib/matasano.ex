e32bc49791e0c8fce7a473a1d77c312ce1c12a61
Aaron Bull Schaefer <aaron@elasticdog.com>
2015-03-19 11:56:59 -0700

    Add solution and tests for Set 1 Challenge 1
    http://cryptopals.com/sets/1/challenges/1/

--- lib/matasano.ex
+++ lib/matasano.ex
@@ -1,2 +1,9 @@
 defmodule Matasano do
+  @doc """
+  Converts a hexadecimal `string` into a base 64 encoded string.
+  """
+  @spec hex_to_base64(String.t) :: String.t
+  def hex_to_base64(string) do
+    Base.decode16!(string, case: :mixed) |> Base.encode64()
+  end
 end
--- test/matasano_test.exs
+++ test/matasano_test.exs
@@ -1,7 +1,9 @@
 defmodule MatasanoTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "convert hex to base64" do
+    hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    assert Matasano.hex_to_base64(hex) == base64
   end
 end
