baf09cb9a50da614f1cc75a0ec5e197b20a5dba0
Aaron Bull Schaefer <aaron@elasticdog.com>
2015-03-20 15:47:52 -0700

    Add module documentation and some doctests

--- lib/matasano.ex
+++ lib/matasano.ex
@@ -1,4 +1,11 @@
 defmodule Matasano do
+  @moduledoc """
+  Provides functions related to solving the
+  [Matasano Crypto Challenges](http://cryptopals.com/).
+  """
+
+  ## Set 1 Challenge 1
+
   @doc """
   Converts a hexadecimal `string` into a base 64 encoded string.
   """
@@ -7,8 +14,18 @@ defmodule Matasano do
     Base.decode16!(string, case: :mixed) |> Base.encode64()
   end
 
+  ## Set 1 Challenge 2
+
   @doc """
   Performs bit-wise XOR (exclusive or) on the data supplied.
+
+  ## Examples
+
+      iex> key = <<28, 1, 17, 0, 31, 1, 1, 0, 6, 26, 2, 75, 83, 83, 80, 9, 24, 28>>
+      iex> Matasano.fixed_xor("hit the bull's eye", key)
+      "the kid don't play"
+      iex> Matasano.fixed_xor("the kid don't play", key)
+      "hit the bull's eye"
   """
   @spec fixed_xor(iodata, iodata) :: binary
   def fixed_xor(a, b), do: :crypto.exor(a, b)
--- test/matasano_test.exs
+++ test/matasano_test.exs
@@ -1,5 +1,6 @@
 defmodule MatasanoTest do
   use ExUnit.Case, async: true
+  doctest Matasano
 
   test "convert hex to base64" do
     hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
