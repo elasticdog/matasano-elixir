b1fd6db400ec0ac1d26cab032d3f84713a36fc6f
Aaron Bull Schaefer <aaron@elasticdog.com>
2015-03-23 15:35:13 -0700

    Reorganize things a bit
    I feel like the code will eventually need to be split up into multiple
    modules, but organizing based on the chronological order of challenges
    doesn't seem to make total sense. So, I've made the tests include the
    specific challenge that they're solving, and the code will grow
    organically moving forward.

--- lib/matasano.ex
+++ lib/matasano.ex
@@ -4,34 +4,6 @@ defmodule Matasano do
   [Matasano Crypto Challenges](http://cryptopals.com/).
   """
 
-  ## Set 1 Challenge 1
-
-  @doc """
-  Converts a hexadecimal `string` into a base 64 encoded string.
-  """
-  @spec hex_to_base64(String.t) :: String.t
-  def hex_to_base64(string) do
-    Base.decode16!(string, case: :mixed) |> Base.encode64()
-  end
-
-  ## Set 1 Challenge 2
-
-  @doc """
-  Performs bit-wise XOR (exclusive or) on the data supplied.
-
-  ## Examples
-
-      iex> key = <<28, 1, 17, 0, 31, 1, 1, 0, 6, 26, 2, 75, 83, 83, 80, 9, 24, 28>>
-      iex> Matasano.fixed_xor("hit the bull's eye", key)
-      "the kid don't play"
-      iex> Matasano.fixed_xor("the kid don't play", key)
-      "hit the bull's eye"
-  """
-  @spec fixed_xor(iodata, iodata) :: binary
-  def fixed_xor(a, b), do: :crypto.exor(a, b)
-
-  ## Set 1 Challenge 3
-
   # https://en.wikipedia.org/wiki/Letter_frequency
   # http://www.data-compression.com/english.html
   @english_distribution %{
@@ -63,6 +35,28 @@ defmodule Matasano do
     "Z" => 0.0007836,
     " " => 0.1918182,
   }
+
+  @doc """
+  Converts a hexadecimal `string` into a base 64 encoded string.
+  """
+  @spec hex_to_base64(String.t) :: String.t
+  def hex_to_base64(string) do
+    Base.decode16!(string, case: :mixed) |> Base.encode64()
+  end
+
+  @doc """
+  Performs bit-wise XOR (exclusive or) on the data supplied.
+
+  ## Examples
+
+      iex> key = <<28, 1, 17, 0, 31, 1, 1, 0, 6, 26, 2, 75, 83, 83, 80, 9, 24, 28>>
+      iex> Matasano.fixed_xor("hit the bull's eye", key)
+      "the kid don't play"
+      iex> Matasano.fixed_xor("the kid don't play", key)
+      "hit the bull's eye"
+  """
+  @spec fixed_xor(iodata, iodata) :: binary
+  def fixed_xor(a, b), do: :crypto.exor(a, b)
 
   @doc """
   Decrypts `ciphertext` by testing all possible single byte values as a key to
@@ -186,8 +180,6 @@ defmodule Matasano do
       :math.sqrt(left_value * right_value) + acc
     end
   end
-
-  ## Set 1 Challenge 4
 
   @doc """
   Reads file at `path` and returns the line that is most likely to be English
--- test/matasano_test.exs
+++ test/matasano_test.exs
@@ -2,14 +2,14 @@ defmodule MatasanoTest do
   use ExUnit.Case, async: true
   doctest Matasano
 
-  test "convert hex to base64" do
+  test "set 1 challenge 1 - convert hex to base64" do
     hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
     assert Matasano.hex_to_base64(hex) == base64
   end
 
-  test "fixed xor" do
+  test "set 1 challenge 2 - fixed xor" do
     a = Base.decode16!("1c0111001f010100061a024b53535009181c", case: :lower)
     b = Base.decode16!("686974207468652062756c6c277320657965", case: :lower)
     output = Base.decode16!("746865206b696420646f6e277420706c6179", case: :lower)
@@ -17,14 +17,14 @@ defmodule MatasanoTest do
     assert Matasano.fixed_xor(a, b) == output
   end
 
-  test "single-byte xor cipher" do
+  test "set 1 challenge 3 - single-byte xor cipher" do
     ciphertext = Base.decode16!("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", case: :lower)
     plaintext = "Cooking MC's like a pound of bacon"
 
     assert Matasano.decrypt_single_byte_xor(ciphertext) == plaintext
   end
 
-  test "detect single-character xor" do
+  test "set 1 challenge 4 - detect single-character xor" do
     data = Path.join("data", "4.txt")
     plaintext = "Now that the party is jumping\n"
 
